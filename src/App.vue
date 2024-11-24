040ced7d38ebe78e94240e1075d8d6f3914a3432
Konstantin <163849606+it2konst@users.noreply.github.com>
2024-11-24 13:01:03 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div class="w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14 bg-slate-400">
+    <header>
+      <div>
+        <img src="/logo.png" alt="Logo" />
+      </div>
+    </header>
+
+    <h1>111111111</h1>
+  </div>
+</template>
+
+<style></style>
